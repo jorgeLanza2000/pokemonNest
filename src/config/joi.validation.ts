b483edda2dea8ac861e618345aec1a420cc06d6d
jorgeLanza2000 <jorgefernandolanza@gmail.com>
2026-03-14 14:10:13 -0600

    [FEAT] prueba para puerto 2

--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -2,6 +2,5 @@ import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
   MONGODB: Joi.required(),
-  PORT: Joi.number().default(3005),
   DEFAULT_LIMIT: Joi.number().default(7),
 });
